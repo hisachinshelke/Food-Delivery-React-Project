cf33286a83140021c84220240688c176eff0f982
Sachin Shelke <48155218+hisachinshelke@users.noreply.github.com>
2024-07-23 13:03:35 +0530

    App chunking Lazy with Suspense implementation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-import About from "./components/About";
+//import About from "./components/About";
 import Contact from "./components/Contact";
 import Errorcp from "./components/Errorcp";
 import RestaurantMenu from "./components/RestaurantMenu";
+
+//chunking - bundle has enough feature
+//code splitting
+//lazy/ondemand loading
+//dynamic bundling
+const Grocery = lazy(() => import("./components/Grocery"));
+
+const About = lazy(() => import("./components/About"));
 
 const AppLayout = () => {
   return (
@@ -23,8 +31,23 @@ const appRouter = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: "/", element: <Body /> },
-      { path: "/about", element: <About /> },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>About Us page loading...</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
       { path: "/contact", element: <Contact /> },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Grocery Loading...</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
       { path: "/restaurant/:resId", element: <RestaurantMenu /> },
     ],
     errorElement: <Errorcp />,
---
+++ src/components/Grocery.js
@@ -0,0 +1,10 @@
+const Grocery = () => {
+  return (
+    <div>
+      <h1>This is Grocery component</h1>
+      <h3>This is lot of children components</h3>
+    </div>
+  );
+};
+
+export default Grocery;
